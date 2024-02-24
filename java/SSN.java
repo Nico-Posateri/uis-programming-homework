6623537a358c68682a83428292e0d62abedf94f0
Nico Posateri <141705409+Nico-Posateri@users.noreply.github.com>
2024-02-24 02:13:58 -0600

    Create SSN.java

---
+++ java/SSN.java
@@ -0,0 +1,13 @@
+/*
+A program that prompts the user to enter a Social Security number
+in the format DDD-DD-DDDD, where D is a digit, and denies invalid formats.
+*/
+
+import java.util.Scanner; // Imports the Scanner class
+
+public class Phone {
+        public static void main(String[] args) {
+		            Scanner input = new Scanner(System.in); // Creates a new Scanner object
+	            	System.out.print("Enter an SSN: "); // Asks user for a letter
+        }
+}
