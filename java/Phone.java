ec9347c6c9b186aa102596560de489856b7c370c
Nico Posateri <141705409+Nico-Posateri@users.noreply.github.com>
2024-02-24 02:06:40 -0600

    Update Phone.java

--- java/Phone.java
+++ java/Phone.java
@@ -9,7 +9,33 @@ import java.util.Scanner; // Imports the Scanner class
 public class Phone {
         public static void main(String[] args) {
 		Scanner input = new Scanner(System.in); // Creates a new Scanner object
-		System.out.println("Enter a letter: ");
+		System.out.println("Enter a letter: "); // Asks user for a letter
+		char letter = input.next().charAt(0);
+		letter = Character.toUpperCase(letter); // Converts letter to uppercase, negates need for lowercase cases
+
+		int number = 0; // A new value will be applied below, depending on chosen letter
+		if(Character.isLetter(letter)) { // If the user's input is a letter . . .
+			if(letter >= 'W') // Letter is W, X, Y, or Z
+				number = 9;
+			else if(letter >= 'T') // Letter is T, U, or V
+				number = 8;
+			else if(letter >= 'P') // Letter is P, Q, R, or S
+				number = 7;
+			else if(letter >= 'M') // etcetera . . .
+				number = 6;
+			else if(letter >= 'J')
+				number = 5;
+			else if(letter >= 'G')
+				number = 4;
+			else if(letter >= 'D')
+				number = 3;
+			else if(letter >= 'A')
+				number = 2;
+			System.out.print("The corresponding number is " + number);
+		}
+		else {
+			System.out.print(letter + " is an invalid input"); // Prints for non-letter inputs
+		}
 	}
 
 /*///////////////////////// First attempt, before Chapter 4 lecture: //////////////////////////
@@ -19,7 +45,7 @@ import java.util.Scanner; // Imports the Scanner class
 public class Phone {
         public static void main(String[] args) {
                 Scanner input = new Scanner(System.in); // Creates a new Scanner object
-		System.out.println("Enter a letter: ");
+		System.out.print("Enter a letter: ");
 		char letter = input.next().charAt(0); // Accepts user input for a single letter
 
                 input.close(); // Closes the Scanner input
@@ -28,47 +54,47 @@ public class Phone {
 		if(letter == 'A' || letter == 'a' || 
 		   letter == 'B' || letter == 'b' || 
 		   letter == 'C' || letter == 'c') {
-			System.out.println("The corresponding number is 2");
+			System.out.print("The corresponding number is 2");
 		}
 		else if(letter == 'D' || letter == 'd' || 
 			letter == 'E' || letter == 'e' || 
 			letter == 'F' || letter == 'f') {
-			System.out.println("The corresponding number is 3");
+			System.out.print("The corresponding number is 3");
 		}
 		else if(letter == 'G' || letter == 'g' || 
 			letter == 'H' || letter == 'h' || 
 			letter == 'I' || letter == 'i') {
-			System.out.println("The corresponding number is 4");
+			System.out.print("The corresponding number is 4");
 		}
 		else if(letter == 'J' || letter == 'j' || 
 			letter == 'K' || letter == 'k' || 
 			letter == 'L' || letter == 'l') {
-			System.out.println("The corresponding number is 5");
+			System.out.print("The corresponding number is 5");
 		}
 		else if(letter == 'M' || letter == 'm' || 
 			letter == 'N' || letter == 'n' || 
 			letter == 'O' || letter == 'o') {
-			System.out.println("The corresponding number is 6");
+			System.out.print("The corresponding number is 6");
 		}
 		else if(letter == 'P' || letter == 'p' || 
 			letter == 'Q' || letter == 'q' || 
 			letter == 'R' || letter == 'r' || 
 			letter == 'S' || letter == 's') {
-			System.out.println("The corresponding number is 7");
+			System.out.print("The corresponding number is 7");
 		}
 		else if(letter == 'T' || letter == 't' || 
 			letter == 'U' || letter == 'u' || 
 			letter == 'V' || letter == 'v') {
-			System.out.println("The corresponding number is 8");
+			System.out.print("The corresponding number is 8");
 		}
 		else if(letter == 'W' || letter == 'w' || 
 			letter == 'X' || letter == 'x' || 
 			letter == 'Y' || letter == 'y' || 
 			letter == 'Z' || letter == 'z') {
-			System.out.println("The corresponding number is 9");
+			System.out.print("The corresponding number is 9");
 		}
 		else {
-			System.out.println(letter + " is an invalid input");
+			System.out.print(letter + " is an invalid input");
 		}
     }
 }
