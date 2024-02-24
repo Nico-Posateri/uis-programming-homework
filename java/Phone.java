743e8bdba3b667f435d6c26d8ced57cf11542237
Nico Posateri <141705409+Nico-Posateri@users.noreply.github.com>
2024-02-23 23:16:31 -0600

    Update Phone.java

--- java/Phone.java
+++ java/Phone.java
@@ -7,5 +7,13 @@ The program uses the international standard letter / number mapping found on the
 import java.util.Scanner; // Imports the Scanner class
 
 public class Phone {
+        public static void main(String[] args) {
+		            Scanner input = new Scanner(System.in); // Creates a new Scanner object
+		            System.out.print("Enter a letter: ");
+		            char letter = input.next().charAt(0); // Accepts user input for a single letter
 
+                input.close(); // Closes the Scanner input
+		
+		            System.out.print("The corresponding number is "); // Prints the number corresponding with the user's letter
+        }
 }
