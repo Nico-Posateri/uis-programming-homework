d139aac6c3f9d6561bffa081a34bec90570f1cac
Nico Posateri <141705409+Nico-Posateri@users.noreply.github.com>
2024-02-24 14:46:45 -0600

    Update SSN.java

--- java/SSN.java
+++ java/SSN.java
@@ -5,7 +5,7 @@ the format DDD-DD-DDDD, where D is a digit, and denies invalid formats.
 
 import java.util.Scanner; // Imports the Scanner class
 
-public class Phone {
+public class SSN {
         public static void main(String[] args) {
 		Scanner input = new Scanner(System.in); // Creates a new Scanner object
 	        System.out.print("Enter an SSN: "); // Asks user for a valid SSN
