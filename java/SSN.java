0f2b3f11a435ab879b202abf366bb9832a7ea92e
Nico Posateri <141705409+Nico-Posateri@users.noreply.github.com>
2024-02-24 13:37:02 -0600

    Update SSN.java

--- java/SSN.java
+++ java/SSN.java
@@ -9,5 +9,10 @@ public class Phone {
         public static void main(String[] args) {
 		Scanner input = new Scanner(System.in); // Creates a new Scanner object
 	        System.out.print("Enter an SSN: "); // Asks user for a valid SSN
+		String ssn = input.nextLine();
+
+		// Implement check for proper length, format
+
+		System.out.print(ssn + " is a valid SSN"); // Currently says anything is valid
         }
 }
