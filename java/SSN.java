385f22ce0c8b7ed9e51d73a2ae64eff90470e2c0
Nico Posateri <141705409+Nico-Posateri@users.noreply.github.com>
2024-02-24 14:32:14 -0600

    Update SSN.java

--- java/SSN.java
+++ java/SSN.java
@@ -14,12 +14,18 @@ public class Phone {
 		input.close(); // Closes the Scanner input
 
 		boolean isValid =
-			(ssn.length() == 11);
-		        // digit at char 0, 1, 2
-		        // '-' at char 3
-		        // digit at char 4, 5
-		        // '-' at char 6
-		        // digit at char 7, 8, 9, 10
+			(ssn.length() == 11) &&
+		        (Character.isDigit(ssn.charAt(0)) &&
+                        (Character.isDigit(ssn.charAt(1)) &&
+                        (Character.isDigit(ssn.charAt(2)) &&
+		        (ssn.charAt(3) == '-') &&
+		        (Character.isDigit(ssn.charAt(4)) &&
+                        (Character.isDigit(ssn.charAt(5)) &&
+		        (ssn.charAt(6) == '-') &&
+		        (Character.isDigit(ssn.charAt(7)) &&
+                        (Character.isDigit(ssn.charAt(8)) &&
+                        (Character.isDigit(ssn.charAt(9)) &&
+                        (Character.isDigit(ssn.charAt(10));
 
 		// Currently validates any string of exactly 11 characters
 		System.out.print(ssn + " is " + ((isValid) ? "a valid" : "an invalid") + " SSN");
