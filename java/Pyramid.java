477a6fa6464d2aa405c9ef436637b2ce3440523a
Nico Posateri <141705409+Nico-Posateri@users.noreply.github.com>
2024-03-03 14:47:31 -0600

    Update Pyramid.java

--- java/Pyramid.java
+++ java/Pyramid.java
@@ -11,11 +11,15 @@ public class Pyramid {
 		// Asks user to enter how many lines long the pyramid is
 		System.out.print("Enter the number of lines, between 1 and 15: ");
 		int linesRequested = input.nextInt(); // Accepts the number
-		
+
 		input.close(); // Closes the Scanner input
 
 		// While the number of lines to print is less than the user's input number, print lines . . .
 		for (int linesPrinted = 1; linesPrinted <= linesRequested; linesPrinted++) {
+			// Prints the spaces for current line
+			for (int space = linesRequested - linesPrinted; space >= 1; space--) {
+				System.out.print("  ");
+			}
 			// Prints descending numbers for current line
 			for (int descNum = linesPrinted; descNum >= 2; descNum--) {
 				System.out.print(descNum + " ");
@@ -23,10 +27,6 @@ public class Pyramid {
                         // Prints ascending numbers for current line
 			for (int ascNum = 1; ascNum <= linesPrinted; ascNum++) {
 				System.out.print(ascNum + " ");
-			}
-			// Prints the spaces for current line
-			for (int space = linesRequested - linesPrinted; space >= 1; space--) {
-				System.out.print("   ");
 			}
                         // Finishes printing the current line
 			System.out.println();
