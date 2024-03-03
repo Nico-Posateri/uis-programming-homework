e5d9315dd0e09d323e76bddd1d614c9a88fc51d3
Nico Posateri <141705409+Nico-Posateri@users.noreply.github.com>
2024-03-03 13:53:14 -0600

    Update Pyramid.java

--- java/Pyramid.java
+++ java/Pyramid.java
@@ -8,14 +8,17 @@ import java.util.Scanner; // Imports the Scanner class
 public class Pyramid {
         public static void main(String[] args) {
 	        Scanner input = new Scanner(System.in); // Creates a new Scanner object
-		System.out.print("Enter the number of lines: "); // Asks user to enter how many lines long the pyramid is
-		int lines = input.nextLine(); // Accepts the number
+		// Asks user to enter how many lines long the pyramid is
+		System.out.print("Enter the number of lines, between 1 and 15: ");
+		int linesRequested = input.nextInt(); // Accepts the number
 		
 		input.close(); // Closes the Scanner input
 
-		// Logic . . .
-
-                // Prints the pyramid
-		System.out.println();
+		// While the number of lines to print is less than the user's input number, print lines . . .
+		for (int linesPrinted = 1; linesPrinted <= linesRequested; linesPrinted++) {
+			// Fill content of lines . . .
+			
+			System.out.println();
+		}
         }
 }
