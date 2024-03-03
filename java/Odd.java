5edbebb0958c0004ce380d586d902a63fd155c14
Nico Posateri <141705409+Nico-Posateri@users.noreply.github.com>
2024-03-03 13:26:00 -0600

    Update Odd.java

--- java/Odd.java
+++ java/Odd.java
@@ -8,7 +8,7 @@ public class Odd {
         public static void main(String[] args) {
 	        Scanner input = new Scanner(System.in); // Creates a new Scanner object
 		System.out.print("Enter a string: "); // Asks user to enter a string
-		String string = input.nextLine();
+		String string = input.nextLine(); // Accepts the string
 		
 		input.close(); // Closes the Scanner input
 
@@ -16,8 +16,8 @@ public class Odd {
 		String oddCharacters = "";
 
 		// Logic for determining and holding odd characters
-		for (a; b; c) {
-			// Calc
+		for (int i = 0; i < string.length(); i += 2) {
+			oddCharacters += string.charAt(i);
 		}
 
 		// Prints the odd characters from the entered string
