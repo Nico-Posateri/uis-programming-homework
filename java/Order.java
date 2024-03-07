f7c80a858715d0391499c182b292acf5abd382c5
Nico Posateri <141705409+Nico-Posateri@users.noreply.github.com>
2024-03-07 16:45:06 -0600

    Create Order.java

---
+++ java/Order.java
@@ -0,0 +1,12 @@
+/*
+A program that displays three numbers in increasing order, after
+prompting the user to enter the three numbers.
+*/
+
+import java.util.Scanner; // Imports the Scanner class
+
+public class Order {
+  public static void displaySortedNumbers(double num1, double num2, double num3) {
+    // . . .
+  }
+}
