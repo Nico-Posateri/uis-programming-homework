a4d0f537f672ed003122d4358dc5534ce5e81d32
Nico Posateri <141705409+Nico-Posateri@users.noreply.github.com>
2024-03-08 10:23:00 -0600

    Create Password.java

---
+++ java/Password.java
@@ -0,0 +1,16 @@
+/*
+A program that prompts the user to enter a password
+and displays "Valid Password" if the rules are followed,
+or "Invalid Password" otherwise. The rules are:
+  - A password must have at least eight characters
+  - A password must contain only letters and digits
+  - A password must contain at least two digits
+*/
+
+import java.util.Scanner; // Imports the Scanner class
+
+public class Password {
+  public static void main(String[] args) {
+    // . . .
+  }
+}
