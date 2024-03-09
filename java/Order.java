c73ce24ab918f5ccd163f904a4725c55d9c7794f
Nico Posateri <141705409+Nico-Posateri@users.noreply.github.com>
2024-03-09 02:05:21 -0600

    Update Order.java

--- java/Order.java
+++ java/Order.java
@@ -9,18 +9,33 @@ public class Order {
 	// Requests user input of 3 numbers, then displays the result of the displaySortedNumbers method:
 	public static void main(String[] args) {
         	Scanner input = new Scanner(System.in); // Creates a new Scanner object
-		System.out.print("Enter 3 numbers: "); // Requests user input of 3 numbers
-		double n1 = input.nextDouble(); // Accepts user input for the first number
-        	double n2 = input.nextDouble(); // Accepts user input for the second number
-        	double n3 = input.nextDouble(); // Accepts user input for the third number
+		System.out.print("Enter 3 numbers, separated by a space: "); // Requests user input of 3 numbers
+		double num1 = input.nextDouble(); // Accepts user input for the first number
+        	double num2 = input.nextDouble(); // Accepts user input for the second number
+        	double num3 = input.nextDouble(); // Accepts user input for the third number
 
         	input.close(); // Closes the Scanner input
         
-        	displaySortedNumbers(n1, n2, n3); // Displays the result of the method below
+        	displaySortedNumbers(num1, num2, num3); // Displays the result of the method below
     	}
     	// Sorts the numbers input above in ascending order, returns result to above call:
     	public static void displaySortedNumbers(double num1, double num2, double num3) {
 		// Determine which of n1, n2, n3 is the highest, lowest...
+		if (num1 > num2) {
+			double temp = num1;
+			num1 = num2;
+			num2 = temp;
+		}
+		if (num2 > num3) {
+			double temp = num2;
+			num2 = num3;
+			num3 = temp;
+		}
+		if (num1 > num2) {
+			double temp = num1;
+			num1 = num2;
+			num2 = temp;
+		}
 
         	System.out.println(num1 + " " + num2 + " " + num3); // Orders numbers in ascending order
     	}
