811aa5ceb9440ed4b65c2295e3b20296e08130ad
Nico Posateri <141705409+Nico-Posateri@users.noreply.github.com>
2024-03-31 17:36:25 -0500

    Update Sorting.java

--- java/Sorting.java
+++ java/Sorting.java
@@ -7,8 +7,23 @@ import java.util.Scanner; // Imports the Scanner class
 public class Sorting {
         public static void main(String[] args) {
 		Scanner input = new Scanner(System.in); // Creates a new Scanner object
-	        System.out.print("Enter list: "); // Asks user for a valid SSN
-
-          // Accept list input
+	        System.out.print("Enter list: "); // Asks user to enter a list
+		int[] list = new int[input.nextInt()];
+		for (int i = 0; i < lost.length; i++) {
+			list[i] = input.nextInt();
+		}
 
 		input.close(); // Closes the Scanner input
+
+		System.out.println(isSorted(list) ? "The list is already sorted." : "The list is not sorted.");
+	}
+
+	public static boolean isSorted(int[] list) {
+		for (int i = 0; i < list.length - 1; i++) {
+			if (list[i] > list[i + 1]) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
