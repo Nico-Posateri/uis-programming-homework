f05cf47cb731b3b018ef5a69ffefaf7ca781123a
Nico Posateri <141705409+Nico-Posateri@users.noreply.github.com>
2024-03-31 17:15:51 -0500

    Create Sorting.java

---
+++ java/Sorting.java
@@ -0,0 +1,14 @@
+/*
+A program that prompts a user to enter a list and displays whether the list is sorted or not.
+*/
+
+import java.util.Scanner; // Imports the Scanner class
+
+public class Sorting {
+        public static void main(String[] args) {
+		Scanner input = new Scanner(System.in); // Creates a new Scanner object
+	        System.out.print("Enter list: "); // Asks user for a valid SSN
+
+          // Accept list input
+
+		input.close(); // Closes the Scanner input
