3222a38e5c2d44863bfaee80c97055ed33c39501
Nico Posateri <141705409+Nico-Posateri@users.noreply.github.com>
2024-04-14 12:04:41 -0500

    Update Location.java

--- java/Location.java
+++ java/Location.java
@@ -8,10 +8,25 @@ storing the indices in a two-dimensional array as int types.
 
 2) Write a method named locateLargest for returning the location of the largest element in a two-dimensional array.
 Note that the return value is an instance of Location.
+         public static Location locateLargest (double [ ][ ]  a)
 */
 
 import java.util.Scanner; // Imports the Scanner class
 
 public class Location {
-  //
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in); // Creates a new Scanner object
+        // Asks user to enter the number of rows and columns in the array
+		System.out.print("Enter the number of rows and columns in the array: ");
+        int rows = input.nextInt(); // Accepts a number for the rows
+        int columns = in.nextInt(); // Accepts a number for the columns
+        // Asks user to enter the array
+		System.out.print("Enter the array: ");
+
+		input.close(); // Closes the Scanner input
+    }
+
+    public static Location locateLargest (double [ ][]  a) {
+        // To-Do...
+    }
 }
