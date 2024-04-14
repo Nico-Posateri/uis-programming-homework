41771b7f2b75405e84d3cf486c0c3bd519842dbc
Nico Posateri <141705409+Nico-Posateri@users.noreply.github.com>
2024-04-14 11:58:37 -0500

    Update Location.java

--- java/Location.java
+++ java/Location.java
@@ -1,6 +1,17 @@
 /*
-A program that displays three numbers in increasing order, after
-prompting the user to enter the three numbers.
+A program that prompts the user to enter a two-dimensional array and displays the location of
+the largest element in the array by the following instructions:
+
+1) Design a class named Location for locating a maximal value and its location in a two-dimensional array,
+containing a public data field maxValue storing the maximal value as a double type and a public data fields row and column
+storing the indices in a two-dimensional array as int types.
+
+2) Write a method named locateLargest for returning the location of the largest element in a two-dimensional array.
+Note that the return value is an instance of Location.
 */
 
 import java.util.Scanner; // Imports the Scanner class
+
+public class Location {
+  //
+}
