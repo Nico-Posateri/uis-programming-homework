ee532843a9a6d0e46e270c2887ba40fc1247d737
Nico Posateri <141705409+Nico-Posateri@users.noreply.github.com>
2024-04-14 15:33:00 -0500

    Update Location.java

--- java/Location.java
+++ java/Location.java
@@ -33,7 +33,7 @@ public class Location {
 	System.out.printf("The location of the largest element is __ at (%d, %d)."); // Prints the location of the largest element
     }
 
-    public static Location locateLargest (double[][] a) {
+    public static int[] locateLargest(double[][] a) {
         int[] location = new int[]{ 0, 0 };
 	double largest = a[0][0];
 	for (int i = 0; i < a.length; i++) {
